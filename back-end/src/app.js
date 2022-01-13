5cd9387f41997e132798da26e522eaa7090b27ad
Mikito Saarna <mikitosaarna@gmail.com>
2022-01-12 21:36:49 -0600

    Added comments back into app.js file

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(function(req, res, next) {
     // change the link of the 2nd argument to the location that the client is at
     res.header("Access-Control-Allow-Origin", "https://restaurant-reservation-application.vercel.app");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
